23178c008877562c53ac6ba6fde5f01a53f3f729
Laurens Espada <lambda@Laurenss-MacBook-Air.fritz.box>
2023-06-23 00:19:01 +0200

    updated slaters condition file

--- NDCS assignment 4/question1_a_slaters_condition.m
+++ NDCS assignment 4/question1_a_slaters_condition.m
@@ -1,11 +1,5 @@
-aircraft;
-
-
-
-zero = zeros(n,m*Tfinal);
-
-% Matrix and vector for linear equality constraint
-
+% Import standard function definitions
+question1_a_function_definitions;
 
 % Find a point in the constraint set, which is necessarily in its
 % relative interior
@@ -15,29 +9,7 @@ u_test_lower = -12 < u;
 u_test_upper = u < 12;
 
 if ~nnz(u_test_lower==0) && ~nnz(u_test_upper==0)
-    disp("Slater's condition holds")
-end
-
-function [A_h, b_h] equality_constraints(A1,A2,A3,A4,B1,B2,B3,B4,x01,x02,x03,x04,Tfinal)
-    C1 = Ci(A1,B1,Tfinal); v1 = vi(A1,x01,Tfinal);
-    C2 = Ci(A2,B2,Tfinal); v2 = vi(A2,x02,Tfinal);
-    C3 = Ci(A3,B3,Tfinal); v3 = vi(A3,x03,Tfinal);
-    C4 = Ci(A4,B4,Tfinal); v4 = vi(A4,x04,Tfinal);
-    
-    A_h = [C1,   -C2,  zero, zero ;
-     zero,  C2,  -C3,  zero ;
-     zero, zero, C3,   C4   ];
-    b_h = [v1 - v2 ; v2 - v3 ; v3 - v4];
-end
-
-function out = Ci(Ai,Bi,Tfinal)
-    n = size(Ai,1); m = size(Bi,2);
-    out = zeros(n,m*Tfinal);
-    for t=1:Tfinal
-        out(:, m*t+1:m*(t+1)) = Ai^(Tfinal-t)*Bi;
-    end
-end
-
-function out = vi(Ai,x0i,Tfinal)
-    out = Ai^Tfinal * x0i;
+    disp("Slater's condition holds.")
+else
+    disp("Slater's condition does not hold.")
 end
